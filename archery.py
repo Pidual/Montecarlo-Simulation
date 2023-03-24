34c52a09cb2f3751f330996b3767420ef87f1868
Carlos <73614255+Pidual@users.noreply.github.com>
2023-03-24 08:45:31 -0500

    Add files via upload

---
+++ archery.py
@@ -0,0 +1,17 @@
+import random
+
+class Archer:
+    gender = None #0 para hombre y 1 para mujer
+    resisntecia = None #35 ± 10
+    experiencia = None #Entero 10
+    suerte = None #1 a 3
+    puntucion_individual = 666
+    
+    def __init__(self): #aca para que se creen pseudo aletoriamente
+        ri = random.random()
+        if random.random() < 0.5:
+            gender = 0
+        else:
+            gender = 1
+        
+    
