6081205102c2b9185c5c045b9310777df31e0e56
Carlos <73614255+Pidual@users.noreply.github.com>
2023-03-24 09:09:22 -0500

    Update archery.py

--- archery.py
+++ archery.py
@@ -1,23 +1,31 @@
 import random
 
+
 class Archer:
-    gender = None #0 para hombre y 1 para mujer
-    resisntecia = None #35 ± 10
-    experiencia = None #Entero 10
-    suerte = None #1 a 3
-    puntucion_individual = 666
-    
-    def __init__(self): #aca para que se creen pseudo aletoriamente
+
+    gender = None  # 0 para hombre y 1 para mujer
+    endurance = None  # 35 ± 10
+    experiencia = None  # Entero 10
+    suerte = None  # 1 a 3
+
+    def __init__(self):  # aca para que se creen pseudo aletoriamente
+        #Escoje el genero con el metodo montecarlo 0.5 de chance cada genero
         ri = random.random()
         if ri < 0.5:
             gender = 0
         else:
             gender = 1
-
-        if ri <= 0.3: #este metodo matematicamente tiene como un 0.002 de probabilidad extra la suerte 3 o 2
+        #Resistencia 35 ± 10 
+        if ri < 0.5:
+            endurance = 35 - random.randint(1,10)
+        else:
+            endurance = 35 + random.randint(1,10)
+            
+        if ri <= 0.3:  # este metodo matematicamente tiene como un 0.002 de probabilidad extra la suerte 3 o 2
             suerte = 1
-        else if ri > 0.3 and ri <= 0.66
+        elif ri > 0.3 and ri <= 0.66:
             suerte = 2
         else:
             suerte = 3
-
+        experiencia = 10
+        individual_score = 0
